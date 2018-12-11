4ead7ac88052fafc340dd48c18a7ba3de2d6ce1d
chunghoony <chunghoony@users.noreply.github.com>
2018-12-10 23:03:58 -0500

    Update and rename amazon_dataset.py to amazonDataset.py

--- amazonDataset.py
+++ amazonDataset.py
@@ -5,7 +5,7 @@ import pandas as pd
 import gzip
 from sklearn.model_selection import train_test_split, cross_val_score
 
-class amazonDataset:
+class amazonDataset():
 	def parse(path):
 	    g = gzip.open(path, 'r')
 	    for l in g:
@@ -14,14 +14,15 @@ class amazonDataset:
 	def getDF(path):
 	    i = 0
 	    df = {}
-	    for d in parse(path):
+	    for d in amazonDataset.parse(path):
 	        df[i] = d
 	        i += 1
 	    ''' Return python dataframe of the data '''
 	    return pd.DataFrame.from_dict(df, orient='index')
 
-#how to
-#load the amazon dataset
-amazon = getDF('reviews_Musical_Instruments_5.json.gz')
-#split train and test data #overall refers to the ratings
-x_train, x_test, y_train, y_test = train_test_split(amazon.reviewText, amazon.overall, random_state=0)
+if __name__ == '__main__':
+	#load the amazon dataset
+	amazon = amazonDataset.getDF('reviews_Musical_Instruments_5.json.gz')
+	#split train and test data #overall refers to the ratings
+	x_train, x_test, y_train, y_test = train_test_split(amazon.reviewText, amazon.overall, random_state=0)
+	print('Successfully loaded the dataset into train and test set.')
