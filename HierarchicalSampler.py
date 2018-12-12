39e4f129329d6daaf4c752a9e37ba38ae5205f56
Daniel L Chen <dalchen@umich.edu>
2018-12-12 15:38:32 -0500

    Finish hierarchical sampling

--- HierarchicalSampler.py
+++ HierarchicalSampler.py
@@ -11,7 +11,7 @@ import numpy as np
 import random
 import logging, sys
 
-from pdb import set_trace
+from pprint import pprint
 
 class Node:
     def __init__(self, node_id):
@@ -62,6 +62,16 @@ class HierarchicalSampler(Sampler):
         self.revealed = set()
         self.admissible_set = set()
         self.beta = 2.0
+
+        # "Tuning phase": exhaust all training data first
+        self.tuning_phase = True
+        for _ in range(X_train.shape[0]):
+            self.sample()
+        self.tuning_phase = False
+        print('Tuning phase complete. Ready to use sampler.')
+
+
+
         '''
         self.labels = {self.root: np.random.choice(self.classes)}
         node = self.tree.get_node(self.root)
@@ -214,8 +224,9 @@ class HierarchicalSampler(Sampler):
             (1) choose v in Pruning with probability proportional to w_v.
             This is similar to random sampling.'''
             # Normalize weights
-            weights = [self.nodes[node_id].weight for node_id in self.pruning]
-            v = np.random.choice(self.pruning, size=None, replace=True, p=weights)
+            pruning = list(self.pruning.keys())
+            weights = [self.nodes[node_id].weight for node_id in pruning]
+            v = np.random.choice(pruning, size=None, replace=True, p=weights)
             return v
         
         def method_2():
@@ -230,25 +241,26 @@ class HierarchicalSampler(Sampler):
     def _pick_sample_id_from_node(self, node_id):
         '''Pick a random point (node id) z from subtree T_{node_id}.
        
-        Modifies:
-        self.nodes[node_id].all_sampled
+        Should only be called after all training samples have been "revealed".
+        Therefore, the id returned should only be an unlabled one.
+        Return None if all samples under this node have been sampled.
         '''
         # TODO: use margin sampling to find a weighted random sample for better performance.
         # For now, follow the algorithm.
-
-        # First always choose training (labeled) data.
-        # If none are training (labeled), then pick random from unlabeled
+        # Adjustment for tuning phase: always prefer training data first
         sample_ids = []
         for leaf in self.nodes[node_id].subtree_leaves: 
             sample_id = leaf.id
             if sample_id not in self.revealed:
-                # Always prefer training data
                 if not self._is_unlabeled(sample_id):
+                    assert self.tuning_phase
                     self.revealed.add(sample_id)
                     return sample_id
                 sample_ids.append(sample_id)
 
-        if len(sample_ids) > 0:
+        # tuning phase: MUST use training (labeled) data.
+        # not tuning phase: need to retry other node.
+        if self.tuning_phase or len(sample_ids) == 0:
             return None
 
         unlabeled_id = random.choice(sample_ids)
@@ -272,24 +284,6 @@ class HierarchicalSampler(Sampler):
                 node.num_revealed += 1
             return
 
-        # "Tuning phase": exhaust all training data first
-        # This conditional should be true ONLY during the tuning phase
-        # Update nodes from leaf to root
-        # This phase should only execute once
-        while len(self.revealed) < self.X_train.shape[0]:
-            z_id = len(self.revealed)
-            self.revealed.add(z_id)
-            z_label = self.y_merged[z_id]
-            update_counts(z_label, z_id, self.root)
-        if len(self.revealed) == self.X_train.shape[0]:
-            # TODO: Do _update procedure and change self.pruning
-            # If this is not done, this function will end up in an infinite loop.
-            # so do this asap.
-            self._update(list(range(self.X_merged.shape[0])), self.labels)
-            pass
-
-
-        # After the tuning phase
         v_id, z_id = -1, -1  # sentinel values
         while True:
             # Loop section begin: keep looping because node v may have 
@@ -302,6 +296,7 @@ class HierarchicalSampler(Sampler):
                 continue
             # Loop section end.
             # Valid z when reach here
+            print('z: '+str(z_id))
             z_label = self.y_merged[z_id]
             update_counts(z_label, z_id, v_id)
             self._update(self._get_upward_path(z_id, v_id), [z_label])
@@ -335,9 +330,15 @@ class HierarchicalSampler(Sampler):
 
         # Math functions here
         def p_LB(v_id, label):
+            # no observations
+            if len(self.nodes[v_id].class_to_instances) == 0:
+                return 0
             vl = (v_id, label)
             return max(p_vl[vl]-delta[vl] , 0.0)
         def p_UB(v_id, label):
+            # no observations
+            if len(self.nodes[v_id].class_to_instances) == 0:
+                return 1
             vl = (v_id, label)
             return min(p_vl[vl]+delta[vl], 1.0)
 
@@ -347,85 +348,88 @@ class HierarchicalSampler(Sampler):
         # Update admissible set A
         def is_admissible(v_id, label):
             vl = (v_id, label)
-            # TODO: shouldn't happen, think tomorrow morning
-            if vl not in p_vl_LB:
-                print("vl not in p_vl_LB")
-                return False
             lb = p_vl_LB[vl]
             min_other = float('inf')
             for lp in self.labels:
                 if lp != label:
                     vlp = (v_id, lp)
-                    if vlp in p_vl_UB:
-                        min_other = min(min_other, 1 - p_vl_UB[vlp])
-                    # TODO: shouldn't happen, think tomorrow morning
-                    else:
-                        print("v_id,lp NOT IN UB DICTIONARY")
-                        import pdb; pdb.set_trace()
+                    min_other = min(min_other, 1 - p_vl_UB[vlp])
+            #print('1 - '+str(lb)+' < 2 * '+str(min_other))
             return 1 - lb < self.beta * min_other
 
         for node_id in update_nodes:
             for label in update_labels:
                 if is_admissible(node_id, label):
+                    #print('node ' + str(node_id) + ' label '+str(label) + ' is admissible')
                     self.admissible_set.add((node_id, label))
 
-        import pdb; pdb.set_trace()
-
-        # TODO: bottom up parsing
-        raise NotImplementedError
 
         # Compute epsilon_tilda_vl and s_v values bottom-up
         epsilon_tilda_vl = {}  # (node_id, int label) --> float
-        s_v = {}  # node_id --> P' and L' represented as {v_id: label}
+        s_v = {}  # node_id --> [score, P' and L' represented as {v_id: label}]
         # Helper function to process nodes in tree bottom-up
         def bottom_up_compute():
             visited = set()
 
             def visit(node_id):
-                '''Return score and dict of {node_id: label}.'''
+                '''Mutates entries in epsilon_tilda_vl and s_v.'''
                 visited.add(node_id)
 
                 node = self.nodes[node_id]
-                left_score, right_score = 0, 0
-                left_mappings, right_mappings = {}, {}
+
                 if node.left and node.left.id not in visited:
-                    left_score, left_labels = visit(node.left.id)
+                    visit(node.left.id)
                 if node.right and node.right.id not in visited:
-                    right_score, right_labels = visit(node.right.id)
+                    visit(node.right.id)
 
                 # All children have been visited
                 has_admissible = False
-                best_epsilon, best_mappings = -float('inf'), {node_id}
-                for label in node.class_to_instances:
+                for label in self.labels:
                     vl = (node_id, label)
                     if vl in self.admissible_set:
                         has_admissible = True
                         epsilon_tilda_vl[vl] = 1 - p_vl[vl]
                     else:
                         epsilon_tilda_vl[vl] = 1
-                    if epsilon_tilda_vl[vl] < best_epsilon:
-                        best_epsilon, best_label = epsilon_tilda_vl[vl], set([label])
-                if has_admissible:
-                    assert best_label is not None
+                best_label = min((l for l in self.labels), key=lambda l: epsilon_tilda_vl[(node_id,l)])
+                best_score = epsilon_tilda_vl[(node_id, best_label)]
+                s_v[node_id] = [best_score, {node_id: best_label}]
+                if has_admissible and not node.is_leaf:
                     w_v = node.weight
-                    children_score, children_labels = 0, 
-                    if left_label: children_score += (node.left.weight / w_v) * left_score
-                    if right_label: children_score += (node.right.weight / w_v) * right_score
-
-                assert best_label is not None
-
-                return best_epsilon, best_label
+                    children_score, children_pruning = 0, {}
+                    
+                    if node.left:
+                        left_id = node.left.id
+                        left_score, left_pruning = s_v[left_id]
+                        #print('node.left: ' + str(node.left.weight / w_v * left_score))
+                        children_score += node.left.weight / w_v * left_score
+                        children_pruning.update(left_pruning)
+                    if node.right:
+                        right_id = node.right.id
+                        right_score, right_pruning = s_v[right_id]
+                        #print('node.right: ' + str(node.right.weight / w_v * right_score))
+                        children_score += node.right.weight / w_v * right_score
+                        children_pruning.update(right_pruning)
+                    
+                    if children_score < best_score:
+                        s_v[node_id] = [children_score, children_pruning]
+                return
 
             for node_id in self.nodes:
                 if node_id not in visited:
-                    s_v[node_id] = visit(node_id)
+                    visit(node_id)
             return
 
-        #bottom_up_compute()
+        bottom_up_compute()
 
+        original_pruning = list(self.pruning.keys())
+        for v_id in original_pruning:
+            score, PL_prime = s_v[v_id]
+            del self.pruning[v_id]
+            self.pruning.update(PL_prime)
+        #pprint(self.pruning)
 
-        print("TODO: IMPLEMENT THIS.")
-        raise NotImplementedError
+        return
         
 
 if __name__ == '__main__':
@@ -443,9 +447,4 @@ if __name__ == '__main__':
     X_unlabeled, y_unlabeled = X_train_base[training_size:], y_train_base[training_size:]
 
     hs = HierarchicalSampler(X_train, y_train, X_unlabeled, y_unlabeled)
-    for _ in range(X_train.shape[0]):
-        print(hs.sample())
-    '''
-    print("After all X_train")
     print(hs.sample())
-    '''
